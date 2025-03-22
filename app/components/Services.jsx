c8ddaef4e33b16ae2272e7d2b90153dc6601d41e
Wenceslaus Obimka <obimkasomto@gmail.com>
2025-03-22 15:23:08 +0100

    done with development

--- app/components/Services.jsx
+++ app/components/Services.jsx
@@ -2,29 +2,69 @@ import { assets, serviceData } from "@/assets/assets";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { motion } from "motion/react";
 
 const Services = () => {
   return (
-    <div id="services" className="w-full px-[12%] py-10 scroll-mt-20">
-      <h4 className="text-center text-lg font-ovo mb-2">What I offer</h4>
-      <h2 className="font-ovo text-center text-5xl">My Services</h2>
-      <p className="text-center max-w-2xl mx-auto mt-5 mb-12 font-ovo">
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1 }}
+      id="services"
+      className="w-full px-[12%] py-10 scroll-mt-20"
+    >
+      <motion.h4
+        initial={{ y: -20, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
+        transition={{ delay: 0.3, duration: 0.5 }}
+        className="text-center text-lg font-ovo mb-2"
+      >
+        What I offer
+      </motion.h4>
+      <motion.h2
+        initial={{ y: -20, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
+        transition={{ delay: 0.5, duration: 0.5 }}
+        className="font-ovo text-center text-5xl"
+      >
+        My Services
+      </motion.h2>
+      <motion.p
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ delay: 0.7, duration: 0.5 }}
+        className="text-center max-w-2xl mx-auto mt-5 mb-12 font-ovo"
+      >
         I am a Frontend developer with many years of on field experience,
         building websites and web apps for notable organisations.
-      </p>
-      <div className="grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] gap-6 my-10">
+      </motion.p>
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ delay: 0.9, duration: 0.6 }}
+        className="grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] gap-6 my-10"
+      >
         {serviceData.map(({ description, icon, link, title }, i) => (
-          <div key={i} className="border border-gray-400 rounded-lg px-8 py-12 hover:shadow-black cursor-pointer hover:bg-lightHover hover:-translate-y-1 duration-500 dark:hover:bg-darkHover dark:hover:shadow-white">
+          <motion.div
+          whileHover={{scale: 1.05}}
+            key={i}
+            className="border border-gray-400 rounded-lg px-8 py-12 hover:shadow-black cursor-pointer hover:bg-lightHover hover:-translate-y-1 duration-500 dark:hover:bg-darkHover dark:hover:shadow-white"
+          >
             <Image src={icon} alt={title} className="w-10" />
-            <h3 className="text-lg my-4 text-gray-700 dark:text-white">{title}</h3>
-            <p className="text-sm text-gray-600 leading-5 dark:text-white/80">{description}</p>
+            <h3 className="text-lg my-4 text-gray-700 dark:text-white">
+              {title}
+            </h3>
+            <p className="text-sm text-gray-600 leading-5 dark:text-white/80">
+              {description}
+            </p>
             <Link href={link} className="flex items-center gap-2 text-sm mt-5">
-              Read more <Image src={assets.right_arrow} alt="" className="w-4" />
+              Read more{" "}
+              <Image src={assets.right_arrow} alt="" className="w-4" />
             </Link>
-          </div>
+          </motion.div>
         ))}
-      </div>
-    </div>
+      </motion.div>
+    </motion.div>
   );
 };
 
